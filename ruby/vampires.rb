139290a0c1a34c6e849859ea42e9c80125e2a0fe
Theron Cross <theroncross@gmail.com>
2016-03-22 14:36:24 -0500

    Fix sunshine allergy to output 'probably...'

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -21,11 +21,11 @@ def detect_vampire()
 	end
 
 	result = ""
-	if name == "Drake Cula" || name == "Tu Fang" || allergies == "sunshine"
+	if name == "Drake Cula" || name == "Tu Fang"
 		result = "Definitely a vampire"
 	elsif (2016 - birth_year != age) && (likes_garlic == "no") && (wants_insurance == "no")
 		result = "Almost certainly a vampire"
-	elsif (2016 - birth_year != age) && (likes_garlic == "no") || (wants_insurance == "no")
+	elsif allergies == "sunshine" || (2016 - birth_year != age) && (likes_garlic == "no") || (wants_insurance == "no")
 		result = "Probably a vampire"
 	elsif (2016 - birth_year == age) && (likes_garlic == "yes") || (wants_insurance == "yes")
 		result = "Probably not a vampire"
