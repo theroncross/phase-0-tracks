63bc7fe6a8f25ca5a634b5758fe424dfeb144cca
Theron Cross <theroncross@gmail.com>
2016-03-22 14:34:32 -0500

    Check for allergies to sunshine

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -14,8 +14,14 @@ def detect_vampire()
 	p "Would you like to enroll in our companies health insurance? (yes/no)"
 	wants_insurance = gets.chomp.downcase
 
+	allergies = ""
+	until allergies == "sunshine" || allergies == "done" do
+		p "Please list your allergies, one at a time. When you are done, type 'done'."
+		allergies = gets.chomp.downcase
+	end
+
 	result = ""
-	if name == "Drake Cula" || name == "Tu Fang"
+	if name == "Drake Cula" || name == "Tu Fang" || allergies == "sunshine"
 		result = "Definitely a vampire"
 	elsif (2016 - birth_year != age) && (likes_garlic == "no") && (wants_insurance == "no")
 		result = "Almost certainly a vampire"
