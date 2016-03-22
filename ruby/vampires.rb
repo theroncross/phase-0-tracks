b2e1916e00a81c4c0c69d56b6b9877cc5299e58c
Theron Cross <theroncross@gmail.com>
2016-03-22 13:42:10 -0500

    Add interview questions

---
+++ ruby/vampires.rb
@@ -0,0 +1,15 @@
+p "What is your name?"
+name = gets.chomp
+
+p "What is your age?"
+age = gets.chomp
+
+p "What year were you born?"
+birth_year = gets.chomp
+
+p "Our company cafeteria serves garlic bread. Should we order some for you?"
+garlic_bread = gets.chomp
+
+p "Would you like to enroll in our companies health insurance?"
+insurance = gets.chomp
+
