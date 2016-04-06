d53cd4abaf9c57ea266e738355be2404492a2845
Theron Cross <theroncross@gmail.com>
2016-04-06 09:49:13 -0500

    Add instance variables to Santa class

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,7 +1,20 @@
 # Santa class with speak and eat methods
 class Santa
-  def initialize
-    puts 'Initalizing Santa instance...'
+  def initialize(gender, ethnicity)
+    @gender = gender
+    @ethnicity = ethnicity
+    @reindeer_ranking = %w(
+      'Rudolph'
+      'Dasher'
+      'Dancer'
+      'Prancer'
+      'Vixen'
+      'Comet'
+      'Cupid'
+      'Donner'
+      'Blitzen'
+    )
+    @age = 0
   end
 
   def speak
@@ -13,6 +26,24 @@ class Santa
   end
 end
 
-santa = Santa.new
-santa.speak
-santa.eat_milk_and_cookies 'oatmeal raising cookie'
+santas = []
+genders = %w(
+  'agender'
+  'female'
+  'bigender'
+  'male'
+  'female'
+  'gender fluid'
+  'N/A'
+)
+ethnicities = %w(
+  'black'
+  'Latino'
+  'white'
+  'Japanese-African'
+  'prefer not to say'
+  'Mystical Creature (unicorn)'
+  'N/A'
+)
+
+20.times { santas << Santa.new(genders.sample, ethnicities.sample) }
