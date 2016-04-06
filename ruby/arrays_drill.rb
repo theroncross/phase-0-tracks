af9c0b9359da677c92e35ae5d6f7c43203807ce5
Theron Cross <theroncross@gmail.com>
2016-04-06 09:12:04 -0500

    Add style changes according to Rubocop guidelines

--- ruby/arrays_drill.rb
+++ ruby/arrays_drill.rb
@@ -1,33 +1,33 @@
 # Release 2
-def build_array1 (first, second, third)
+def build_array1(first, second, third)
   [first, second, third]
 end
-p build_array1(3,"text",1)
+p build_array1(3, 'text', 1)
 
 def add_to_array(var1, var2)
   var1 << var2
 end
-p add_to_array([1,2,3], 4)
-p add_to_array(["blue", "red",5], 8)
-p add_to_array(build_array1("1st", "3rd", "2nd"), "4th")
+p add_to_array([1, 2, 3], 4)
+p add_to_array(['blue', 'red', 5], 8)
+p add_to_array(build_array1('1st', '3rd', '2nd'), '4th')
 
 # Release 1
 teas = []
 p teas
 
-teas += ["Earl Grey", "Darjeeling", "Chamomile", "Rose hips", "Mint"]
+teas += ['Earl Grey', 'Darjeeling', 'Chamomile', 'Rose hip', 'Mint']
 p teas
 
 teas.delete_at(2)
 p teas
 
-teas.insert(2, "peach")
+teas.insert(2, 'peach')
 p teas
 
-teas.delete("Earl Grey")
+teas.delete('Earl Grey')
 p teas
 
-looking_for = "Darjeeling"
+looking_for = 'Darjeeling'
 result = teas.include?(looking_for)
 if result
   p "Teas includes #{looking_for}"
@@ -35,6 +35,6 @@ else
   p "Teas does not include #{looking_for}"
 end
 
-coffees = ["French Roast", "Bold", "Latte"]
+coffees = ['French Roast', 'Bold', 'Latte']
 drinks = teas + coffees
 p drinks
