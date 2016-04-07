9ef0dc3b7101bd037d92263e0eddde5339809699
Theron Cross <theroncross@gmail.com>
2016-04-07 09:40:15 -0500

    Add mixin Shout module and Soccer mom and kid classes

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,13 +1,39 @@
 # A shout module
+# module Shout
+#   def self.yell_angrily(words)
+#     words + '!!!' + ':('
+#   end
+
+#   def self.yell_happily(words)
+#     words + '!!!' + ':)'
+#   end
+# end
+
+# puts Shout.yell_angrily 'Dad blast it'
+# puts Shout.yell_happily 'Golly gumby'
+
+# A Shout module for mixin in to classes
 module Shout
-  def self.yell_angrily(words)
-    words + '!!!' + ':('
+  def yell_angrily(words)
+    puts "#{words}!!! >:|"
   end
 
-  def self.yell_happily(words)
-    words + '!!!' + ':)'
+  def yell_happily(words)
+    puts "#{words}!!! :)"
   end
 end
 
-puts Shout.yell_angrily 'Dad blast it'
-puts Shout.yell_happily 'Golly gumby'
+# Soccer moms with Shout mixins
+class SoccerMom
+  include Shout
+end
+
+# Soccer kids with Shout mixins
+class SoccerKid
+  include Shout
+end
+
+angry_soccer_mom = SoccerMom.new
+angry_soccer_mom.yell_angrily 'Oh come on ref'
+happy_soccer_kid = SoccerKid.new
+happy_soccer_kid.yell_happily 'Woohoo'
