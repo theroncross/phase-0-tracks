47ba4cb809c8728c2242955bee89dd01dffabee4
Theron Cross <theroncross@gmail.com>
2016-04-11 18:09:53 -0500

    Add function to find matching key-value pairs in objects

--- js/algos.js
+++ js/algos.js
@@ -16,6 +16,21 @@ function longest(arr) {
   return temp_longest;
 }
 
+// Find a Key-Value Match
+// FOR EACH key in the first object
+// IF the value in each object matches
+// Return true immediately
+// Return false after checking the last key in object 1
+
+function matchingPairs(obj1, obj2) {
+  for(let property in obj1) {
+    if(obj1[property] == obj2[property]) {
+      return true;
+    }
+  }
+  return false;
+}
+
 const names = ["Theron", "Jennifer", "Ethan"];
 const cities = ["Los Angeles", "Houston", "Chicago", "New York"];
 const numbers = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"];
@@ -23,3 +38,11 @@ const numbers = ["one", "two", "three", "four", "five", "six", "seven", "eight",
 console.log(longest(names));
 console.log(longest(cities));
 console.log(longest(numbers));
+
+const theron = {name: 'Theron', weight: 175, gender: 'male'};
+const jennifer = {name: 'Jennifer', weight: 130, gender: 'female'};
+const ethan = {name: 'Ethan', weight: 130, gender: 'male'};
+
+console.log(matchingPairs(theron, jennifer));
+console.log(matchingPairs(jennifer, ethan));
+console.log(matchingPairs(theron, ethan));
