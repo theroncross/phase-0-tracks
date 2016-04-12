342b412e76f4085b8c30af5e776033e56e09b75b
Theron Cross <theroncross@gmail.com>
2016-04-11 19:07:55 -0500

    Add function for generating arrays of random strings

--- js/algos.js
+++ js/algos.js
@@ -31,6 +31,33 @@ function matchingPairs(obj1, obj2) {
   return false;
 }
 
+// Generate random test data
+// initialize an empty array
+// FOR the given number of iterations
+// Initialize an empty string to store the random word
+// generate a random length and store it in a variable
+// FOR the length number of iterations
+// randomly generate a number in the arabic letters unicode range
+// Turn that number in the character that belongs to that code and concantenate it onto the random word variable
+// Push the word into the array
+// Return the array
+
+function randomStrings(quantity) {
+  const words = [];
+  for(let i = 0; i < quantity; i++) {
+    let word = "",
+        charCode = 0,
+        wordLength = Math.floor(10 * Math.random() + 1);
+    for(let j = 0; j < wordLength; j++) {
+      charCode = Math.floor(26 * Math.random() + 97);
+      word += String.fromCharCode(charCode);
+    }
+    words.push(word);
+  }
+  return words;
+}
+
+// driver code for longest
 const names = ["Theron", "Jennifer", "Ethan"];
 const cities = ["Los Angeles", "Houston", "Chicago", "New York"];
 const numbers = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"];
@@ -39,6 +66,7 @@ console.log(longest(names));
 console.log(longest(cities));
 console.log(longest(numbers));
 
+// driver code for matchingPairs
 const theron = {name: 'Theron', weight: 175, gender: 'male'};
 const jennifer = {name: 'Jennifer', weight: 130, gender: 'female'};
 const ethan = {name: 'Ethan', weight: 130, gender: 'male'};
@@ -46,3 +74,13 @@ const ethan = {name: 'Ethan', weight: 130, gender: 'male'};
 console.log(matchingPairs(theron, jennifer));
 console.log(matchingPairs(jennifer, ethan));
 console.log(matchingPairs(theron, ethan));
+
+// driver code for randomStrings
+let arrayLength = 0,
+    arrayOfStrings = [];
+for(let i = 0; i < 10; i++) {
+  arrayLength = Math.floor(7 * Math.random() + 3);
+  arrayOfStrings = randomStrings(arrayLength);
+  console.log(arrayOfStrings);
+  console.log(longest(arrayOfStrings));
+}
