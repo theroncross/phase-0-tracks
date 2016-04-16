c32e52628a2746d64d88b0aea2735e0ed3768b88
Theron Cross <theroncross@gmail.com>
2016-04-15 22:18:46 -0500

    Add horse name color pairs to an object

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,10 +1,16 @@
+// Arrays of colors and names
 var colors = ["red", "blue", "gray", "purple"];
-
 var horseNames = ["Ed", "Clovis", "Fred", "Joe"];
-
+// Push new elements into each
 colors.push("pink");
-
 horseNames.push("Wendy");
-
+// Log the results
 console.log(colors);
 console.log(horseNames);
+// Create an object to store name: color pairs
+var coloredHorses = {};
+// For each name, add a name: color pair to the object
+for (var i = 0; i < horseNames.length; i++) {
+  coloredHorses[horseNames[i]] = colors[i];
+}
+console.log(coloredHorses);
