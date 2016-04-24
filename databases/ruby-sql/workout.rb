e2cf9a73b3c981175efb65086a163c14ee18ab17
Theron Cross <theroncross@gmail.com>
2016-04-24 17:50:22 -0500

    Change driver code to repeat through a full set

--- databases/ruby-sql/workout.rb
+++ databases/ruby-sql/workout.rb
@@ -11,10 +11,7 @@ require_relative './repetition_set'
 # each with their own goal times and elapsed times
 # Each athlete rep is saved individually for reporting later
 module Workout
-  def new_athlete_rep(db, athlete_id, repetition_id, repetition_set_id)
-    # TODO: get best times from database and use to calculate based on effort
-    p 'Goal time (in seconds):'
-    goal_time = gets.chomp
+  def new_athlete_rep(db, athlete_id, repetition_id, repetition_set_id, goal_time)
     p 'Press ENTER to start the timer'
     gets.chomp
     start_time = Time.new.to_f
@@ -30,8 +27,8 @@ module Workout
   # Prints repetition information, filtered by athlete, including date
   # TODO: fix date formatting
   def print_results(db, athlete_name)
-    ath_reps = db.execute 'SELECT athletes.name, athlete_repetitions.elapsed_time, repetition_sets.date FROM athletes JOIN athlete_repetitions ON athletes.id = athlete_repetitions.athlete_id JOIN repetition_sets ON athlete_repetitions.repetition_set_id = repetition_sets.id WHERE athletes.name = (?)', [athlete_name]
-    ath_reps.each { |row| p row }
+    ath_reps = db.execute 'SELECT athletes.name, repetitions.distance, repetitions.effort, athlete_repetitions.elapsed_time, repetition_sets.date FROM athletes JOIN athlete_repetitions ON athletes.id = athlete_repetitions.athlete_id JOIN repetition_sets ON athlete_repetitions.repetition_set_id = repetition_sets.id JOIN repetitions ON repetition_sets.repetition_id = repetitions.id WHERE athletes.name = (?)', [athlete_name]
+    ath_reps.each { |row| p "#{row[0]} ran #{row[1]}m at #{row[2]}% effort in #{row[3]}s on #{row[4]}" }
   end
 end
 
@@ -40,5 +37,9 @@ include DB, Workout
 db = DB.create_database 'spring2016'
 athlete = Athlete.choose db
 repetition_set = RepetitionSet.choose db
-Workout.new_athlete_rep db, athlete[0], repetition_set[1], repetition_set[0]
+p 'Goal time (in seconds):'
+goal_time = gets.chomp
+repetition_set[3].times do
+  Workout.new_athlete_rep db, athlete[0], repetition_set[1], repetition_set[0], goal_time
+end
 Workout.print_results db, athlete[1]
